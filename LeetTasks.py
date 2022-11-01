bf4a7b1416165fb5cd33cbc6b3159e1118dcb55f
Abdullah-Mazhar <100299495+Abdullah-Mazhar@users.noreply.github.com>
2022-11-01 18:32:23 +0500

    Tasks

--- LeetTasks.py
+++ LeetTasks.py
@@ -22,7 +22,7 @@
 #             print([i, j])
 #             break
 
-#  Optimize Way
+#  (Optimize Way) Palindrome or not
 # def IsPalindrome(x: int):
 #     temp = x
 #     if x == 0:
@@ -70,3 +70,23 @@
 # x = 10201
 # print(IsPalindrome(x))
 
+# Roman to integer (Task 13)
+def romanToInt(s: str) -> int:
+    word = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, }
+    num = 0
+    i = 0
+    while i < len(s):
+        if s[i] in word:
+            if i < len(s)-1:
+                if (s[i] == 'I' and s[i+1] == 'V') or (s[i] == 'I' and s[i+1] == 'X') or (s[i] == 'X' and s[i+1] == 'L')\
+                or (s[i] == 'X' and s[i+1] == 'C') or (s[i] == 'C' and s[i+1] == 'D') or (s[i] == 'C' and s[i+1] == 'M'):
+                    num = num + (word[s[i+1]] - word[s[i]])
+                    i += 2
+                    continue
+        num = num + word[s[i]]
+        i += 1
+
+    return num
+
+Roman = "MCMXCIV"
+print(romanToInt(Roman))

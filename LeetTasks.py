2e5fc2ca93de0015371957288dc4a8efe3fa0874
Abdullah-Mazhar <100299495+Abdullah-Mazhar@users.noreply.github.com>
2022-11-03 19:02:59 +0500

    Tasks

--- LeetTasks.py
+++ LeetTasks.py
@@ -71,22 +71,50 @@
 # print(IsPalindrome(x))
 
 # Roman to integer (Task 13)
-def romanToInt(s: str) -> int:
-    word = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, }
-    num = 0
-    i = 0
-    while i < len(s):
-        if s[i] in word:
-            if i < len(s)-1:
-                if (s[i] == 'I' and s[i+1] == 'V') or (s[i] == 'I' and s[i+1] == 'X') or (s[i] == 'X' and s[i+1] == 'L')\
-                or (s[i] == 'X' and s[i+1] == 'C') or (s[i] == 'C' and s[i+1] == 'D') or (s[i] == 'C' and s[i+1] == 'M'):
-                    num = num + (word[s[i+1]] - word[s[i]])
-                    i += 2
-                    continue
-        num = num + word[s[i]]
-        i += 1
+# def romanToInt(s: str) -> int:
+#     word = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, }
+#     num = 0
+#     i = 0
+#     while i < len(s):
+#         if s[i] in word:
+#             if i < len(s)-1:
+#                 if (s[i] == 'I' and s[i+1] == 'V') or (s[i] == 'I' and s[i+1] == 'X') or (s[i] == 'X' and s[i+1] == 'L')\
+#                 or (s[i] == 'X' and s[i+1] == 'C') or (s[i] == 'C' and s[i+1] == 'D') or (s[i] == 'C' and s[i+1] == 'M'):
+#                     num = num + (word[s[i+1]] - word[s[i]])
+#                     i += 2
+#                     continue
+#         num = num + word[s[i]]
+#         i += 1
+#
+#     return num
+#
+# Roman = "MCMXCIV"
+# print(romanToInt(Roman))
 
-    return num
 
-Roman = "MCMXCIV"
-print(romanToInt(Roman))
+# Longest Common prefix (Task 14)
+def longestCommonPrefix(strs: list[str]) -> str:
+
+    minimum = min(map(len, strs))
+    word = ""
+
+    for a in range(minimum):
+        for b in range(1, len(strs)):
+            if strs[0][a] == strs[b][a]:
+                ask = True
+                print("True")
+            else:
+                ask = False
+                break
+        if ask:
+            word = word + strs[0][a]
+            print("wo: ", word)
+        else:
+            break
+
+    return word
+
+
+
+st = ["c","acc","ccc"]
+longestCommonPrefix(st)
